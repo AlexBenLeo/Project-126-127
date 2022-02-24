357a97f38be809a4e34924b5b543d151a31bf0c4
AlexBenLeo <85003460+AlexBenLeo@users.noreply.github.com>
2022-02-23 18:58:36 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWrist=0;
+score_leftWrist=0;
+song1_status="";
+song2_status="";
 function preload(){
     song1=loadSound("Peter Pan Song.mp3");
     song2=loadSound("Harry Potter Theme Song.mp3");
@@ -22,15 +25,28 @@ function modelLoaded() {
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        score_leftWrist=results[0].pose.keypoints[9].score;
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
         rightWristX=results[0].pose.rightWrist.x;
         rightWristY=results[0].pose.rightWrist.y;
         console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
-        
+
     }
 }
 function draw(){
     image(video,0,0,600,500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    song1_status=song1.isPlaying();
+    song2_status=song2.isPlaying();
+    if(score_leftWrist>0.2){
+        circle(leftWristX,leftWristY,20);
+        song2.stop();
+        if(song1_status==false){
+            song1.play();
+            document.getElementById("song").innerHTML="Peter Pan Song Is Playing";
+        }
+    }
 }
