8787e6061d84c5b362900406279400f8f8ae2479
AlexBenLeo <85003460+AlexBenLeo@users.noreply.github.com>
2022-02-26 18:56:08 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ leftWristY=0;
 rightWristX=0;
 rightWrist=0;
 score_leftWrist=0;
+score_rightWrist=0;
 song1_status="";
 song2_status="";
 function preload(){
@@ -26,6 +27,7 @@ function gotPoses(results){
     if(results.length>0){
         console.log(results);
         score_leftWrist=results[0].pose.keypoints[9].score;
+        score_rightWrist=results[0].pose.keypoints[10].score;
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
@@ -39,8 +41,17 @@ function draw(){
     image(video,0,0,600,500);
     fill("#FF0000");
     stroke("#FF0000");
+    
     song1_status=song1.isPlaying();
-    song2_status=song2.isPlaying();
+song2_status=song2.isPlaying();
+if(score_rightWrist>0.2){
+    circle(rightWristX,rightWristY,20);
+song1.stop();
+if(song2_status==false){
+    song2.play();
+    document.getElementById("song").innerHTML="Harry Potter Theme Song Is Playing";
+}
+}
     if(score_leftWrist>0.2){
         circle(leftWristX,leftWristY,20);
         song2.stop();
